80c89d77df7276913dd9889ecb9b497ffcb3edc9
Jordi, Pastor <jordi.pastor@sky.uk>
2024-05-21 22:54:45 +0100

    Jordi Pastor resolved exercise2

--- src/README.md
+++ src/README.md
@@ -1,12 +1,26 @@
 # **Client management with react and hooks**
 
-The idea of our company is to create a large application to manage clients. Therefore, client management, from the first session, has been developed using React due to its scalability.
+The idea of our company is to create a large application to manage clients. Therefore, Client management, from the first session, has been redeveloped using React due to its scalability.
 
 Install dependencies: npm install
-Run the app: npm run dev
+Run app: npm run dev
 
 Exercise 1.
 
 The client management app is not working. The client list is not being updated when a new client is added. However, when the website is reloaded, all clients are displayed correctly. Do you know why?
 
-The principal engineer tells you to fix the issue by creating a context for the entire app and saving the list of clients into this context. (Note that this change won't fix the issue, you could test your code using console.log).
+The principal engineer tells you to fix the issue by creating a context for the entire app and saving the list of clients into this context.
+
+Exercise 2.
+
+Even tough, Client management is working now, an improvement has been required. Once a client is added to the list, the fields are not cleaned.
+
+The principal engineer tells you to fix the issue using useReduce, in order to avoid a set of functions, that modify the state, being triggered in a row.
+
+Principal engineer would like to avoid this bad practise:
+setName('');
+setEmail('');
+setPhone('');
+setAddress('');
+
+Extra: create a clear button.
--- src/client/ClientForm.jsx
+++ src/client/ClientForm.jsx
@@ -1,39 +1,67 @@
-import { useState, useContext } from 'react';
+import { useContext, useReducer } from 'react';
 import { ClientContext } from './ClientContext';
 
+const initalFormValue = {
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NAME': {
+      return {
+        ...state,
+        name: action.value
+      }
+    }
+    case 'SET_EMAIL': {
+      return {
+        ...state,
+        email: action.value
+      }
+    }
+    case 'SET_PHONE': {
+      return {
+        ...state,
+        phone: action.value
+      }
+    }
+    case 'SET_ADDRESS': {
+      return {
+        ...state,
+        address: action.value
+      }
+    }
+    case 'CLEAR' : {
+      return initalFormValue;
+    }
+  }
+};
+
 const ClientForm = () => {
-  const { /*clients,*/ setClients } = useContext(ClientContext);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [address, setAddress] = useState('');
+  const [form, dispatchForm] = useReducer(reducer, initalFormValue);
+  const { setClients } = useContext(ClientContext);
   const submitEmployee = () => {
     setClients(prev => [
       ...prev,
       {
-        name,
-        email,
-        phone,
-        address
+        name: form.name,
+        email: form.email,
+        phone: form.phone,
+        address: form.address
       }
     ]);
-    // setClients([
-    //   ...clients,
-    //   {
-    //     name,
-    //     email,
-    //     phone,
-    //     address
-    //   }
-    // ]);
+    dispatchForm({ type: 'CLEAR' });
   };
   return (
     <div className="form">
-      <input type="text" placeholder="Name" onChange={value => setName(value.target.value)} value={name} />
-      <input type="email" placeholder="Email" onChange={value => setEmail(value.target.value)} value={email} />
-      <input type="tel" placeholder="Phone" onChange={value => setPhone(value.target.value)} value={phone} />
-      <input type="text" placeholder="Address" onChange={value => setAddress(value.target.value)} value={address} />
+      <input type="text" placeholder="Name" onChange={value => dispatchForm({ type: 'SET_NAME', value: value.target.value})} value={form.name} />
+      <input type="email" placeholder="Email" onChange={value => dispatchForm({ type: 'SET_EMAIL', value: value.target.value})} value={form.email} />
+      <input type="tel" placeholder="Phone" onChange={value => dispatchForm({ type: 'SET_PHONE', value: value.target.value})} value={form.phone} />
+      <input type="text" placeholder="Address" onChange={value => dispatchForm({ type: 'SET_ADDRESS', value: value.target.value})} value={form.address} />
       <button onClick={() => submitEmployee()}>Add client</button>
+      <button onClick={() => dispatchForm({ type: 'CLEAR' })}>Clear</button>
     </div>
   )
 }
